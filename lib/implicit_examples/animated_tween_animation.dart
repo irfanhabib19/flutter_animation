05b32c2ea6d53884f41284deb8e02dda3b2904c3
habibirfan180 <habibirfan180@gmail.com>
2024-11-28 00:49:17 +0530

    commit

--- lib/implicit_examples/animated_tween_animation.dart
+++ lib/implicit_examples/animated_tween_animation.dart
@@ -8,23 +8,28 @@ class AnimatedTweenAnimation extends StatefulWidget {
 }
 
 class _animatedTween extends State<AnimatedTweenAnimation> {
-  final double size = 200;
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Container(
-        height: 200,
-        width: 200,
-        decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            color: Colors.blue,
-            boxShadow: [
-              BoxShadow(
-                color: Colors.blue.withOpacity(0.5),
-                blurRadius: size,
-                spreadRadius: size / 2,
-              )
-            ]),
+      child: TweenAnimationBuilder(
+        tween: Tween<double>(begin: 0, end: 200),
+        duration: Duration(seconds: 5),
+        builder: (context, size, Widget) {
+          return Container(
+            height: size,
+            width: size,
+            decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.blue,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.blue.withOpacity(0.5),
+                    blurRadius: size,
+                    spreadRadius: size / 2,
+                  )
+                ]),
+          );
+        },
       ),
     );
   }
